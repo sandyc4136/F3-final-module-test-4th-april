27dc3d1c6ea6b05dbf6a1cf1dfc6318c3feb0281
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-04-04 17:33:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let title = document.querySelector(".heading");
+let img1 = document.querySelector(".img");
+let pictureDetails = document.querySelector(".para");
+
+let arr = [];
+
+function getCurrentImageOfTheDay(){
+    let date = new Date().toISOString().split("T")[0];
+   
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=P7ZD2gqjKW1TaQPTnHgFAo1oLyFSADdGFz8X2Qt3&date=${date}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            title.innerHTML = `<h1>NASA's Picture Of The Day: ${data.date} </h1>`;
+            img1.innerHTML = `<img src="${data.hdurl}">`;
+            pictureDetails.innerHTML = `<h3>${data.title} </h3> <p> ${data.explanation} </p>`;
+        })
+    
+    
+}
+getCurrentImageOfTheDay();
+
+let searchDate = document.querySelector("#search-input");
+let btn = document.querySelector("#search");
+btn.addEventListener("click", getImageOfTheDay);
+
+function getImageOfTheDay(e){
+    e.preventDefault();
+    let newDate = searchDate.value;
+    console.log(newDate)
+    
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=P7ZD2gqjKW1TaQPTnHgFAo1oLyFSADdGFz8X2Qt3&date=${newDate}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            title.innerHTML = `<h1>Picture On ${data.date} </h1>`;
+            img1.innerHTML = `<img src="${data.hdurl}">`;
+            pictureDetails.innerHTML = `<h3>${data.title} </h3> <p> ${data.explanation} </p>`;
+        })
+    
+    saveSearch(newDate);
+    addSearchToHistory(newDate);
+}
+let searchResult = document.querySelector("#search-result");
+
+function saveSearch(newDate){
+    arr.push(newDate);
+    localStorage.setItem("searches", JSON.stringify(arr));
+}
+
+function addSearchToHistory(newDate){
+    let searchHistory = JSON.parse(localStorage.getItem("searches"));
+    console.log();
+
+    const myHtml=searchHistory.map((item)=>{
+        const temp= item.split("-")
+        return`
+        <li><a href="#" onclick="getUserDate(${temp[0]}, ${temp[1]} ,${temp[2]})">${temp[0]}-${temp[1]}-${temp[2]}</a> </li>
+        `
+    })
+    
+    searchResult.innerHTML = myHtml.join(" ");
+    console.log(searchDate)
+}
+
+function getUserDate(year, month,date){
+    console.log(year,month,date);
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=P7ZD2gqjKW1TaQPTnHgFAo1oLyFSADdGFz8X2Qt3&date=${year}-${month}-${date}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+           
+            title.innerHTML = `<h1>Picture On ${data.date} </h1>`;
+            img1.innerHTML = `<img src="${data.hdurl}">`;
+            pictureDetails.innerHTML = `<h3>${data.title} </h3> <p> ${data.explanation} </p>`;
+        })
+}
